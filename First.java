8194686676b32b8d5aa0201aa5841e7bd047614e
Disha301 <agrawaldisha301@gmail.com>
2025-01-04 08:56:05 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,13 @@
+public class First {
+	public static void main (String[] args){
+		System.out.print("Hello world");
+		int var =54;
+		int varB =79;   //camelCase
+		System.out.println("multiplication: "+var*varB);
+System.out.println("modulos or remainder : "+var%varB);
+System.out.println("division: "+var/varB);
+		//division operator returns the //quotient and modulo return remainder 
+		
+	}
+}
+
